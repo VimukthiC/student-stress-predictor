eaa7e7ef3e7053f08da12f87f16ae97c7f1872b9
VimukthiC <66357690+VimukthiC@users.noreply.github.com>
2024-01-09 15:46:25 +0000

    create signup page layout using HTML & styled using bootstrap framework

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/home")
 def home():
   return render_template('home.html')
 
 
-@app.route("/signIn")
-def signIn():
-  return render_template('sign_in.html')
+@app.route("/signup")
+def signup():
+  return render_template('signup.html')
 
 
 if __name__ == "__main__":
