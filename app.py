c74108a0527808a03df7d0aa5ddb8d0f7c4585ba
VimukthiC <66357690+VimukthiC@users.noreply.github.com>
2024-01-11 11:30:51 +0000

    Modified

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+Users = [{
+    'username': 'user1',
+    'email': 'user1@gmail.com',
+    'password': '12345'
+}, {
+    'username': 'user2',
+    'email': 'user2@gmail.com',
+    'password': '12345'
+}]
+
 
 @app.route("/home")
 def home():
@@ -10,7 +20,7 @@ def home():
 
 @app.route("/signup")
 def signup():
-  return render_template('signup.html')
+  return render_template('signup.html', users=Users)
 
 
 @app.route("/login")
