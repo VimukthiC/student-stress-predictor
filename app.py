bda4ad56a6febee65b26a469bfe6ee3847c68a07
VimukthiC <pcvimukthi.ousl@gmail.com>
2024-01-19 00:08:39 +0530

    add mysql configaration

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-
-
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Email,ValidationError
+import bcrypt
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'sql12.freesqldatabase.com'
+app.config['MYSQL_USER'] = 'sql12677255'
+app.config['MYSQL_PASSWORD'] = 'I8ZhUeXrEJ'
+app.config['MYSQL_DB'] = 'sql12677255'
+app.secret_key = 'WTF_CSRF_SECRET_KEY'
 
+mysql = MySQL(app)
 
 
 @app.route("/home")
