be4fc4f83d752afd5f8fde2b22a14864e3583dbd
VimukthiC <pcvimukthi.ousl@gmail.com>
2024-01-19 15:21:14 +0530

    add mysql configaration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,ValidationError
 import bcrypt
@@ -6,29 +7,34 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'sql12.freesqldatabase.com'
-app.config['MYSQL_USER'] = 'sql12677255'
-app.config['MYSQL_PASSWORD'] = 'I8ZhUeXrEJ'
-app.config['MYSQL_DB'] = 'sql12677255'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'mydatabase'
 app.secret_key = 'WTF_CSRF_SECRET_KEY'
 
 mysql = MySQL(app)
 
 
-@app.route("/home")
+
+@app.route("/", methods=["POST","GET"])
 def home():
-  return render_template('home.html')
+   return render_template('home.html')
+    
 
-
-@app.route("/signup")
+@app.route("/signup", methods=["POST","GET"])
 def signup():
-  return render_template('signup.html')
+    return render_template("signup.html")
 
 
-@app.route("/login")
+@app.route("/login",  methods = ['GET','POST'])
 def login():
-  return render_template('login.html')
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return "hello world"
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(debug=True)
