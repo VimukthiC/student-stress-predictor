5c2f033bc2b49ef21729464cdb40814e8fbf2ab3
VimukthiC <pcvimukthi.ousl@gmail.com>
2024-01-19 15:36:58 +0530

    Add registration form data to database

--- app.py
+++ app.py
@@ -38,7 +38,21 @@ def home():
 
 @app.route("/signup", methods=["POST","GET"])
 def signup():
-    return render_template("signup.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO users(name, email, password) VALUES (%s, %s, %s)",(name, email, hashed_password))
+        mysql.connection.commit()
+        cursor.close()
+
+        return redirect(url_for('login'))
+    return render_template("signup.html" , form = form)
 
 
 @app.route("/login",  methods = ['GET','POST'])
